033cba7a369e10b6f3654a3edda2957e320d4192
Ievgeniia Vashchenko <dn050493vem@gmail.com>
2023-05-28 12:45:34 +0300

    adds services section (all devices)
    services section WITHOUT carousel and with one card only.
    
    Plus some minor changes in other sections

---
+++ src/js/service-card.js
@@ -0,0 +1,26 @@
+// (() => {
+//   const refs = {
+//     openCardBtn: document.querySelector('[data-card-open]'),
+//     closeCardBtn: document.querySelector('[data-card-close]'),
+//     card: document.querySelector('[data-card]'),
+//     // backdrop: document.querySelector('[data-backdrop]'),
+//   };
+
+//   refs.openCardBtn.addEventListener('click', toggleCard);
+//   refs.closeCardBtn.addEventListener('click', toggleCard);
+
+//   function toggleCard() {
+//     refs.card.classList.toggle('is-opened');
+//     // refs.backdrop.classList.toggle('is-hidden');
+//   }
+// })();
+
+Fancybox.bind('[data-fancybox]', {
+  // Your custom options
+});
+
+// Fancybox.fromSelector('[data-fancybox="card"]');
+
+// Show HTML element
+// Fancybox.show([{ src: '#modal-service-1', type: 'inline' }]);
+// Fancybox.show([{ src: '#modal-service-1', type: 'clone' }]);
---
+++ src/js/slider.js
@@ -0,0 +1,26 @@
+// // core version + navigation, pagination modules:
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// // import Swiper and modules styles
+
+// // init Swiper:
+// const swiper = new Swiper('.swiper', {
+//   // configure Swiper to use modules
+//   modules: [Navigation, Pagination],
+//   // direction: 'vertical',
+//   loop: true,
+//   slidesPerView: 3,
+//   spaceBetween: 8,
+
+//   // breakpoints: {
+//   //   // when window width is >= 320px
+//   //   321: {
+//   //     slidesPerView: 2,
+//   //     spaceBetween: 20,
+//   //   },
+//   // },
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+// });
