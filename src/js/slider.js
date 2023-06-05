9075b37ad553ae7d291ce6fedf21f6c90a06d5ca
Ievgeniia Vashchenko <dn050493vem@gmail.com>
2023-06-05 23:21:01 +0300

    adds slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,37 +1,45 @@
-// // // core version + navigation, pagination modules:
-// // import Swiper, { Navigation, Pagination } from 'swiper';
-// // // import Swiper and modules styles
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import 'swiper/scss/';
+// import 'swiper/scss/navigation';
+// import 'swiper/scss/pagination';
+// import 'swiper/scss/bundle';
+// import Swiper and modules styles
 
-// // // init Swiper:
-// // const swiper = new Swiper('.swiper', {
-// //   // configure Swiper to use modules
-// //   modules: [Navigation, Pagination],
-// //   // direction: 'vertical',
-// //   loop: true,
-// //   slidesPerView: 3,
-// //   spaceBetween: 8,
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  // direction: 'vertical',
+  //   loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  //   loop: true,
+  //   lazy: true,
 
-// //   // breakpoints: {
-// //   //   // when window width is >= 320px
-// //   //   321: {
-// //   //     slidesPerView: 2,
-// //   //     spaceBetween: 20,
-// //   //   },
-// //   // },
+  breakpoints: {
+    // when window width is >= 320px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+  },
 
-// //   // If we need pagination
-// //   pagination: {
-// //     el: '.swiper-pagination',
-// //   },
-// // });
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
 
 // // core version + navigation, pagination modules:
 // // import Swiper, { Navigation, Pagination } from 'swiper';
 // // import Swiper and modules styles
-// // import 'swiper/scss';
-// // import 'swiper/scss/navigation';
-// // import 'swiper/scss/pagination';
-// // import 'swiper/scss/bundle';
+// import 'swiper/scss';
+// import 'swiper/scss/navigation';
+// import 'swiper/scss/pagination';
+// import 'swiper/scss/bundle';
 
 // // swiper/scss/bundle
 
